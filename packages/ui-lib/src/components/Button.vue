75982ada16267578a28d64075f200b3d55b2d8c0
hsiangfeng <19990752+hsiangfeng@users.noreply.github.com>
2024-04-15 17:47:35 +0800

    feat: 🎸 first commit

---
+++ packages/ui-lib/src/components/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+import { defineProps } from 'vue';
+
+defineProps({
+  label: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+  <button class="button">{{ label }}</button>
+</template>
+
+<style scoped>
+</style>
